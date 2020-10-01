f96cd434b8e51322510ef4f77485169ed402475f
Svetlana <svetlanakrbva@rambler.ru>
2020-10-01 14:53:13 +0300

    Добавлено модальное окно

---
+++ source/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const modalBtn = document.querySelector('[data-modal]');
+
+// modal.addEventListener('click', function(){
+//   const modalWindow = document.createElement('div');
+
+// } )
